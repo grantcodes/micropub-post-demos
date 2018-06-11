7020295e19701fc3a4e465781d00e19274aa7b35
Grant Richmond <mail@grant.codes>
2018-06-10 20:33:05 -0400

    make type an array in jf2 to mf2 function

--- src/modules/jf2-to-mf2.js
+++ src/modules/jf2-to-mf2.js
@@ -1,7 +1,7 @@
 export default function(jf2) {
-  let mf2 = { type: 'h-entry', properties: {} }
+  let mf2 = { type: ['h-entry'], properties: {} }
   if (jf2.type) {
-    mf2.type = 'h-' + jf2.type
+    mf2.type = ['h-' + jf2.type]
     delete jf2.type
   }
   Object.keys(jf2).forEach(property => {
